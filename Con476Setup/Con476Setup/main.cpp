1f8b4658dedc8b8cd30ece4760465b6e21560e6e
con <connsteele96@gmail.com>
2019-04-12 00:43:11 -0700

    Added GLAD, TinyObj, Stb_Image
    Also added GLFW and GLM via Nuget pkg manager

---
+++ Con476Setup/Con476Setup/main.cpp
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
+int main() {
+
+
+	return 0; // Exit Main
+}
