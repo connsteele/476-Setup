0f4af8dcff59da52f7474faea8bd8ca212e59dbc
con <connsteele96@gmail.com>
2019-04-12 00:56:33 -0700

    Added base code
    from a variety of sources

--- Con476Setup/Con476Setup/main.cpp
+++ Con476Setup/Con476Setup/main.cpp
@@ -1,11 +1,28 @@
+// 
+#include "math.h"
+// Libraries add to project
 #include <glad/glad.h>
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
+// Base Code from me or Cal Poly Professors
+#include "GLSL.h"
+#include "Program.h"
+#include "Shape.h"
+#include "WindowManager.h"
+#include "MatrixStack.h"
 
-int main() {
+// May not need stuff below
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace std;
+using namespace glm;
+
+int main() 
+{
+
 
 
 	return 0; // Exit Main
